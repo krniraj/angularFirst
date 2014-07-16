ffdb60705223d6396fba5422812891feb52694bd
Niraj <niraj.kumarz@aol.com>
2014-07-15 23:06:28 -0500

    SG-0000: Landing page for login
    Signed-off-by: Niraj <niraj.kumarz@aol.com>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,14 @@ angular.module('firstAppApp')
       'AngularJS',
       'Karma'
     ];
+
+    var ergastAPI = {};
+
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
   });
